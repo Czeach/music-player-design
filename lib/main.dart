c178085193324051b2f2362588f2f12247eee8b0
Ezichi Amarachi <ezichukwuamarachi@gmail.com>
2019-12-19 21:43:11 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,119 +1,218 @@
-import 'package:flutter/cupertino.dart';
+import'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:music_player_mock_up/playlist.dart';
-import 'dart:async';
+import 'albumtitle.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
+  final List<String> song = <String> [
+    'Intro',
+    '1 Milli',
+    'Check Am',
+    'Disturbance',
+    'If',
+    'D & G',
+    'Get To You',
+    'Risky',
+    'Sweet In The Middle',
+    'Fall',
+    'Green Light Riddim',
+    'Big Picture',
+    'One Thing',
+    'Assurance',
+    'Blow My Mind',
+    'Company',
+    'Animashaun'
+  ];
+
+  final List<String> artist = <String> [
+    'Davido',
+    'Davido',
+    'Davido',
+    'Davido Ft. Peruzzi',
+    'Davido',
+    'Davido Ft. Summer Walker',
+    'Davido',
+    'Davido Ft. Popcaan',
+    'Davido Ft. Naira Marley, Wurld & Zlatan',
+    'Davido',
+    'Davido',
+    'Davido Ft. Boogie wit da Hoodie, Dremo & Gunna',
+    'Davido',
+    'Davido',
+    'Davido & Chris Brown',
+    'Davido',
+    'Davido Ft. Yonda'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text(
-            'PLAYLIST',
-            style: TextStyle(
-              fontSize: 18,
-              color: Colors.black87,
-              fontFamily: 'ariel',
-              fontWeight: FontWeight.w300
-            ),
-          ),
-          bottomOpacity: 0.0,
-          elevation: 0.0,
-          centerTitle: true,
-          backgroundColor: Colors.white,
-          leading: IconButton(icon: Icon(Icons.arrow_back_ios),
-              onPressed: () {('');},
-            color: Colors.black87,
-              ),
-          actions: <Widget>[
-            IconButton(
-                icon: Icon(Icons.menu),
-                onPressed: () {('');},
-              color: Colors.black87,
-            ),
-          ],
-        ),
         body: Column(
-          children: <Widget>[
-            Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 2, 0, 0),
-                  child: Container(
-                    width: 70.0,
-                    height: 70.0,
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: AssetImage('images/album.jpg')
+                SizedBox(
+                  height: 28,
+                ),
+                Container(
+                  height: 32,
+                  child: Row(
+                    children: <Widget>[
+                      IconButton(icon: Icon(Icons.arrow_back_ios),
+                        onPressed: () {('');},
+                        color: Colors.black87,
+                      ),
+                      SizedBox(
+                        width: 130,
+                      ),
+                      Text(
+                          'PLAYLIST',
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black87,
+                              fontFamily: 'sans serif',
+                              fontWeight: FontWeight.w400
+                          ),
                         ),
-                        borderRadius: BorderRadius.circular(10.0),
-                        shape: BoxShape.rectangle,
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.grey,
-                              blurRadius: 5.0,
-                              spreadRadius: 0.5,
-                              offset: Offset(0.5, 0.5)
-                          )
-                        ]
+                      SizedBox(
+                        width: 130,
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.menu),
+                        onPressed: () {('');},
+                        color: Colors.black87,
+                      ),
+                    ],
+                  ),
+                ),
+                Album(),
+                Container(
+                  height: 535,
+                  child: ListView.separated(
+                    padding: const EdgeInsets.fromLTRB(16, 8, 8, 0),
+                    itemCount: song.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        height: 32,
+                        color: Colors.white,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              '${song[index]}',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.black,
+                                fontWeight: FontWeight.w400
+                              ),
+                            ),
+                            SizedBox(
+                              height: 2,
+                            ),
+                            Text(
+                              '${artist[index]}',
+                              style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.grey[500]
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                    separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(
                     ),
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.fromLTRB(13, 30, 0, 0),
+                  padding: const EdgeInsets.fromLTRB(10, 2, 8, 2),
                   child: Container(
-                    width: 150.0,
-                    height: 70.0,
-                    child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                    height: 90,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16.0),
+                      color: Colors.green[900],
+                    ),
+                    child: Row(
                       children: <Widget>[
-                        Text(
-                          'A Good Time',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 17,
-                            fontWeight: FontWeight.bold,
-                            //fontFamily: 'times new roman'
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(18, 0, 0, 0),
+                          child: Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(4.0),
+                              color: Colors.green[700],
+                            ),
                           ),
                         ),
-                        SizedBox(
-                          height: 3.0,
-                        ),
-                        Row(
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
-                            Icon(
-                              Icons.shuffle,
-                              color: Colors.black87,
-                            ),
-                            SizedBox(
-                              width: 2.0,
-                            ),
-                            Text(
-                              'Shuffle',
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 14,
-                                  fontFamily: 'Arial'
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(8, 28, 0, 0),
+                              child: Text(
+                                '${song[10]}',
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold
+                                ),
                               ),
                             ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(8, 2, 0, 0),
+                              child: Text(
+                                '${artist[10]}',
+                                style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.white70,
+                                    fontWeight: FontWeight.w400
+                                ),
+                              ),
+                            )
                           ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(16, 2, 0, 0),
+                          child: Row(
+                            children: <Widget>[
+                              IconButton(
+                                  icon:
+                                  Icon(Icons.skip_previous),
+                                  onPressed: () {('');},
+                                iconSize: 38,
+                                color: Colors.grey[200],
+                                  ),
+                              IconButton(
+                                    icon:
+                                    Icon(Icons.pause),
+                                    onPressed: () {('');},
+                                iconSize: 38,
+                                color: Colors.grey[200],
+                                ),
+                              IconButton(
+                                  icon:
+                                  Icon(Icons.skip_next),
+                                  onPressed: () {('');},
+                                iconSize: 38,
+                                color: Colors.grey[200],
+                              ),
+                            ],
+                          ),
                         )
                       ],
                     ),
                   ),
-                ),
+                )
               ],
             ),
-            Playlist(),
-          ],
-        ),
       ),
     );
   }
